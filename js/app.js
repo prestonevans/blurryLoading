750342af5c2bfd7202c7022a917cba41bcafcbd5
prestonevans <89256614+prestonevans@users.noreply.github.com>
2022-08-09 17:13:31 -0600

    my attempt to recreate a blurry loading image

--- js/app.js
+++ js/app.js
@@ -1,2 +1,30 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const backgroundImage = document.querySelector('main');
+const loader = document.querySelector('.loader');
+const intervalTime = 20;
+
+let count = 100;
+
+let intervalId = setInterval(() => {
+  const blurAmount = count / 100;
+  backgroundAnimation(blurAmount);
+  loaderAnimation(blurAmount);
+  if (count === 0) {
+    clearInterval(intervalId);
+    return;
+  }
+  count--;
+}, intervalTime);
+
+function backgroundAnimation(blurAmount) {
+  backgroundImage.style.filter = `blur(${20 * blurAmount}px)`;
+}
+
+function loaderAnimation(blurAmount) {
+  loader.innerText = `${100 - count}%`;
+  if (count <= 75) {
+    loader.style.filter = `blur(${8 * (1 - blurAmount)}px)`;
+  }
+  if (count <= 30) {
+    loader.style.opacity = `${3 * blurAmount}`;
+  }
+}
